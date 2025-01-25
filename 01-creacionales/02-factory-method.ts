d30d5345a1267b383faa997e8e20332373215fe1
teran724 <id2.sistemas@724.com.mx>
2025-01-24 18:30:54 -0600

    patron factory method agregado

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -1,3 +1,4 @@
+import { COLORS } from '../helpers/colors';
 /**
  * ! Factory Method:
  * El patrón Factory Method permite crear objetos sin especificar
@@ -12,3 +13,98 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+// Clase base
+interface Hamburguer{
+    prepare(): void;
+    cook(): void;
+    box(): void;
+}
+
+class ChickenBurger implements Hamburguer{
+    prepare(){
+        console.log('Preparando hamburguesa de pollo', COLORS.blue);
+    }
+    cook(){
+        console.log('Cocinando hamburguesa de pollo');
+    }
+    box(){
+        console.log('Empacando hamburguesa de pollo');
+    }
+}
+
+class BeefBurger implements Hamburguer{
+    prepare(){
+        console.log('Preparando hamburguesa de res', COLORS.red);
+    }
+    cook(){
+        console.log('Cocinando hamburguesa de res');
+    }
+    box(){
+        console.log('Empacando hamburguesa de res');
+    }
+}
+
+class BeansBurger implements Hamburguer{
+    prepare(){
+        console.log('Preparando hamburguesa de frijol', COLORS.green);
+    }
+    cook(){
+        console.log('Cocinando hamburguesa de frijol');
+    }
+    box(){
+        console.log('Empacando hamburguesa de frijol');
+    }
+}
+
+abstract class Restaurant{
+    abstract createHamburguer(): Hamburguer;
+
+    orderHamburguer(){
+        const hamburguer = this.createHamburguer();
+        hamburguer.prepare();
+    }
+}
+
+class ChickenBurgerRestaurant extends Restaurant{
+    override createHamburguer(): Hamburguer{
+        return new ChickenBurger();
+    }
+}
+
+class BeefBurgerRestaurant extends Restaurant{
+    override createHamburguer(): Hamburguer{
+        return new BeefBurger();
+    }
+}
+
+class BeansBurgerRestaurant extends Restaurant{
+    override createHamburguer(): Hamburguer{
+        return new BeansBurger();
+    }
+}
+
+function main(){
+    let restaurant: Restaurant;
+
+    const burguerType = prompt('Ingrese el tipo de hamburguesa (pollo/res/beans): ');
+
+    switch(burguerType){
+        case 'pollo':
+            restaurant = new ChickenBurgerRestaurant();
+            break;
+        case 'res':
+            restaurant = new BeefBurgerRestaurant();
+            break;
+        case 'beans':
+            restaurant = new BeansBurgerRestaurant();
+            break;
+        default:
+            throw new Error('Tipo de hamburguesa no válido');
+            break;
+    }
+
+    restaurant.orderHamburguer();
+}
+
+main();
--- 01-creacionales/02.2-factory-method.ts
+++ 01-creacionales/02.2-factory-method.ts
@@ -36,12 +36,18 @@ interface Report {
 
 class SalesReport implements Report {
   // TODO: implementar el método e imprimir en consola:
+  generate(): void {
+    console.log('Generando reporte de ventas...', COLORS.blue);
+  }
   // 'Generando reporte de ventas...'
 }
 
 class InventoryReport implements Report {
   // TODO: implementar el método e imprimir en consola:
   // 'Generando reporte de inventario...'
+  generate(): void {
+    console.log('Generando reporte de inventario...', COLORS.green);
+  }
 }
 
 // 3. Clase Base ReportFactory con el Método Factory
@@ -59,13 +65,13 @@ abstract class ReportFactory {
 
 class SalesReportFactory extends ReportFactory {
   createReport(): Report {
-    throw new Error('Method not implemented.');
+    return new SalesReport();
   }
 }
 
 class InventoryReportFactory extends ReportFactory {
   createReport(): Report {
-    throw new Error('Method not implemented.');
+    return new InventoryReport();
   }
 }
 

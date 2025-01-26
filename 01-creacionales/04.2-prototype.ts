cbfc14764e146498558bb90eb3bd7f5bbffb5f4a
teran724 <id2.sistemas@724.com.mx>
2025-01-26 01:46:18 -0600

    tarea ejercicio prototype

--- 01-creacionales/04.2-prototype.ts
+++ 01-creacionales/04.2-prototype.ts
@@ -17,13 +17,17 @@ class Pokemon {
   attacks: string[];
 
   constructor(name: string, type: string, level: number, attacks: string[]) {
-    throw new Error('Method not implemented.');
+    this.name = name;
+    this.type = type;
+    this.level = level;
+    this.attacks = attacks;
   }
 
   // Método para clonar el Pokémon
   clone(): Pokemon {
     // Los ataques deben de evitar pasarse por referencia, es decir, no deben de ser el mismo arreglo.
     // Completar: Debe devolver un nuevo Pokémon con los mismos atributos
+    return new Pokemon(this.name, this.type, this.level, [...this.attacks]);
   }
 
   displayInfo(): void {
@@ -49,3 +53,25 @@ class Pokemon {
 
 // basePokemon.displayInfo(); // Aquí no debe de aparecer "Lanzallamas"
 // clone1.displayInfo();
+
+
+function main() {
+  const basePokemon = new Pokemon("Charmander", "Fuego", 1, ["Llamarada", "Arañazo"]);
+  const clone1 = basePokemon.clone();
+  clone1.name = "Charmeleon";
+  clone1.level = 16;
+  clone1.attacks.push("Lanzallamas");
+
+  basePokemon.displayInfo(); // Aquí no debe de aparecer "Lanzallamas"
+  clone1.displayInfo();
+
+  console.log('-------------------------------------------');
+  console.log('Clonando a Charmeleon');
+  const clone2 = clone1.clone();
+  clone2.name = "Charizard";
+  clone2.level = 36;
+  clone2.attacks.push("Vuelo");
+  clone2.displayInfo();
+}
+
+main();

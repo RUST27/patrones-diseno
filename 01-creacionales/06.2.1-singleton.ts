a3e4d871d21ea801631eeb3386a60be3d93047ac
teran724 <id2.sistemas@724.com.mx>
2025-01-27 10:16:48 -0600

    segundo ejemplo de singleton agregado

--- 01-creacionales/06.2.1-singleton.ts
+++ 01-creacionales/06.2.1-singleton.ts
@@ -9,3 +9,11 @@
  *
  * https://refactoring.guru/es/design-patterns/singleton
  */
+
+import { configManager } from "./singleton/config-manager";
+
+configManager.setConfig('apiurl', 'https://api.example.com');
+configManager.setConfig('apikey', '123456');
+configManager.setConfig('timeout', '3000');
+
+console.log(configManager.getConfig('apiurl'));
